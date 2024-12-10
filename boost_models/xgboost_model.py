3d75bd8f2cb64d871e51d2f41a82869f52d4ef06
sobo3038 <sofiabl@bu.edu>
2024-12-10 16:39:49 -0500

    fixed xgboost

--- boost_models/xgboost_model.py
+++ boost_models/xgboost_model.py
@@ -5,39 +5,13 @@ from xgboost import XGBRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.base import BaseEstimator, RegressorMixin
 import numpy as np
-import os
 
-# Get the directory of the current script
-current_dir = os.path.dirname(os.path.abspath(__file__))
-data_path = os.path.join(current_dir, '../data/final_prediction_dataset.csv')
-data = pd.read_csv(data_path)
 
 # Load the dataset
-# data_path = '../data/final_prediction_dataset.csv'
-# data = pd.read_csv(data_path)
+data_path = '../data/final_prediction_dataset.csv'
+data = pd.read_csv(data_path)
 
-#XGB wrapper to target failure of compatibility with different dependencies 
-class CustomXGBRegressor(BaseEstimator, RegressorMixin):
-    def __init__(self, **kwargs):
-        self.model = XGBRegressor(**kwargs)
-
-    def fit(self, X, y):
-        self.model.fit(X, y)
-        return self
-
-    def predict(self, X):
-        return self.model.predict(X)
-
-    def get_tags(self):
-        # Return the default tags from scikit-learn's BaseEstimator, customized as needed
-        return {
-            "non_deterministic": True,
-            "multioutput": False,
-            "requires_positive_X": False,
-            "requires_fit": True,
-        }
 
 # Specify target and features
 target_column = 'gross_adjusted'
@@ -75,7 +49,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Build pipeline
 model_pipeline = Pipeline([
    ('preprocessor', preprocessor),
-   ('regressor', CustomXGBRegressor(n_estimators=100, random_state=42))
+   ('regressor', XGBRegressor(n_estimators=100, random_state=42))
 ])
 
 
@@ -96,3 +70,6 @@ from sklearn.metrics import r2_score
 # Calculate R² score
 r2 = r2_score(y_test, y_pred)
 print(f'R² Score: {r2:.2f}')
+
+
+
